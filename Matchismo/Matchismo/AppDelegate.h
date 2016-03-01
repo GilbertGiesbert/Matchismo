973b0083f29f4b7f025a23b933031b938301cb6b
joern <Gerrit@kaiMac.local>
2016-03-01 11:09:41 +0100

    blank AppCode Project

---
+++ Matchismo/Matchismo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by iOS Entwickler on 01.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ Matchismo/Matchismo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by iOS Entwickler on 01.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
