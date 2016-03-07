9c5d6c679a3b961baad1d6b835f80c3f5e5e25c3
joern <Gerrit@kaiMac.local>
2016-03-07 14:51:52 +0100

    - added face card - added pinch methods - added swipe methods

--- Matchismo/Matchismo/PlayingCardView.m
+++ Matchismo/Matchismo/PlayingCardView.m
@@ -8,6 +8,10 @@
 
 #import "PlayingCardView.h"
 
+@interface PlayingCardView()
+@property (nonatomic) CGFloat faceCardScaleFactor;
+@end
+
 @implementation PlayingCardView
 
 #pragma mark - init
@@ -38,6 +42,20 @@
 }
 
 
+- (void)pinch: (UIPinchGestureRecognizer *)gesture{
+    
+    if( (UIGestureRecognizerStateChanged == gesture.state) ||
+       (UIGestureRecognizerStateEnded == gesture.state)){
+        
+        
+        self.faceCardScaleFactor *= gesture.scale;
+        
+        // prevent accumulation
+        gesture.scale = 1.0;
+    }
+}
+
+
 
 #pragma mark - drawing
 
@@ -57,9 +75,56 @@
     [[UIColor blackColor] setStroke];
     [roundedRect stroke];
     
-    // TODO: draw face or pips
+    if(self.faceUp){
+        
+        UIImage *faceImage = [self loadFaceImageWithRank:[self rankAsString] andSuit:self.suit];
+        if(faceImage){
+            [self drawFace:faceImage];
+        }
+        else{
+            [self drawPips];
+        }
+        
+        [self drawCorners];
+        
+    }
+    else{
+        [[UIImage imageNamed:@"cardback"] drawInRect:self.bounds];
+    }
+}
+
+- (UIImage *)loadFaceImageWithRank:(NSString *)rank andSuit:(NSString *)suit{
     
-    [self drawCorners];
+    if([@"♥️" isEqualToString:suit]){
+        suit = @"H";
+    }
+    else if([@"♣️" isEqualToString:suit]){
+        suit = @"C";
+    }
+    else if([@"♠️" isEqualToString:suit]){
+        suit = @"S";
+    }
+    else if([@"♦️" isEqualToString:suit]){
+        suit = @"D";
+    }
+    
+    return [UIImage imageNamed:[NSString stringWithFormat:@"%@_%@", suit, [self rankAsString]]];
+}
+
+- (void)drawFace:(UIImage *)faceImage{
+    
+    CGRect imageRect = CGRectInset(self.bounds,
+                                    self.bounds.size.width * (1.0 - self.faceCardScaleFactor),
+                                    self.bounds.size.height * (1.0 - self.faceCardScaleFactor));
+    
+    [faceImage drawInRect:imageRect];
+    
+}
+
+- (void)drawPips{
+    
+    
+    
 }
 
 - (void)drawCorners{
@@ -72,15 +137,25 @@
     
     NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit] attributes:@{NSFontAttributeName:cornerFont, NSParagraphStyleAttributeName: paragraphStyle}];
     
+    // upper left corner
     CGRect textBounds;
     textBounds.origin = CGPointMake([self cornerOffset], [self cornerOffset]);
     textBounds.size = [cornerText size];
     [cornerText drawInRect:textBounds];
     
     
+    // upper right coner
+    textBounds.origin = CGPointMake(self.bounds.size.width - textBounds.size.width - [self cornerOffset], [self cornerOffset]);
+    [cornerText drawInRect:textBounds];
+    
+    // bottom left
     CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
-    CGContextRotateCTM(context, M_PI);
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height); // now overall origin is bottom right corner
+    CGContextRotateCTM(context, M_PI); // now overall x-axis goes from right to left, and overall y-axis goes from bottom to top
+    [cornerText drawInRect:textBounds];
+    
+    // bottom right
+    textBounds.origin = CGPointMake([self cornerOffset], [self cornerOffset]);
     [cornerText drawInRect:textBounds];
     
 }
@@ -99,9 +174,24 @@
 - (CGFloat)cornerOffset {return [self cornerRadius] / 3;}
 
 
+#define DEFAULT_FACE_CARD_SCALE_FACTOR 0.90
+
+
 
 
 #pragma mark - getters and setters
+
+@synthesize faceCardScaleFactor = _faceCardScaleFactor;
+
+- (void)setFaceCardScaleFactor: (CGFloat)faceCardScaleFactor{
+    _faceCardScaleFactor = faceCardScaleFactor;
+    [self setNeedsDisplay];
+}
+
+- (CGFloat)faceCardScaleFactor{
+    if(!_faceCardScaleFactor) _faceCardScaleFactor = DEFAULT_FACE_CARD_SCALE_FACTOR;
+    return _faceCardScaleFactor;
+}
 
 
 - (void)setRank:(NSUInteger)rank{
--- Matchismo/Matchismo/SecondViewController.m
+++ Matchismo/Matchismo/SecondViewController.m
@@ -7,20 +7,51 @@
 //
 
 #import "SecondViewController.h"
-#import "PlayingCardView.h";
+#import "PlayingCardView.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface SecondViewController ()
 @property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation SecondViewController
+
+- (Deck *)deck{
+    if(!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)drawRandomPlayingCard{
+    
+    Card *card = [self.deck drawRandomCard];
+    if([card isKindOfClass:[PlayingCard class]]){
+        
+        PlayingCard *playingCard = (PlayingCard *)card; // cast
+        self.playingCardView.rank = playingCard.rank;
+        self.playingCardView.suit = playingCard.suit;
+        
+    }
+    
+}
+
+- (IBAction)swipe:(UISwipeGestureRecognizer *)sender {
+    
+    if(!self.playingCardView.faceUp)
+        [self drawRandomPlayingCard];
+        
+    self.playingCardView.faceUp = !self.playingCardView.faceUp;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.playingCardView.suit = @"♥️";
     self.playingCardView.rank = 13;
+    
+    [self.playingCardView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:self.playingCardView action:@selector(pinch:)]];
 
 }
 
